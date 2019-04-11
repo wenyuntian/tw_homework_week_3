8497f197dd8e681665612ed85b5e3a01017d1bdf
wenyuntian <157325986@qq.com>
2019-04-11 20:41:36 +0800

    finish practice4

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,12 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce() {
+        return String.format("%s I am a Worker. I have a job.", super.introduce(), this.getName(), this.getAge());
+    }
 }
