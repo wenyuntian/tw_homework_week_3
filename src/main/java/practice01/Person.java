87ed8b8d964a8ba5b079f22bdd95c4b161606a38
wenyuntian <157325986@qq.com>
2019-04-11 19:14:19 +0800

    finish practice1

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,27 @@
 package practice01;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", this.name, this.age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
