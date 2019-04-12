95a33e14d9edfd196de08776111f126d1040309b
wenyuntian <157325986@qq.com>
2019-04-12 16:17:15 +0800

    finish practice9

--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,42 @@
 package practice09;
 
 public class Person {
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(int id, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+
+    public boolean equals(Object obj) {
+        Person person = (Person) obj;
+        if(person != null && person.getId() == this.getId()) {
+            return true;
+        }
+        return false;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", this.name, this.age);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,36 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        this.klass = null;
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+    public String introduce() {
+        String personIntroduce = super.introduce();
+
+        if(this.klass == null) {
+            return String.format("%s I am a Teacher. I teach No Class.", personIntroduce);
+        }
+        return String.format("%s I am a Teacher. I teach Class %d.", personIntroduce, this.klass.getNumber());
+    }
+    public String introduceWith(Student student) {
+        Klass studentKlass = student.getKlass();
+        String personIntroduce = super.introduce();
+
+        if(studentKlass.getNumber() == this.klass.getNumber()) {
+            return String.format("%s I am a Teacher. I teach Jerry.", personIntroduce);
+        }
+        return String.format("%s I am a Teacher. I don't teach Jerry.", personIntroduce);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
